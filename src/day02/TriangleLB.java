08c18bc4a24a3d7dc5bd820b7d2218215d55850e
yn0315 <yunu0315@naver.com>
2022-04-23 21:46:26 +0900

    피라미드 별찍기, 숫자

--- src/day02/TriangleLB.java
+++ src/day02/TriangleLB.java
@@ -49,6 +49,31 @@ public class TriangleLB {
         }
     }//end RB
 
+    //피라미드
+    private static void spira(int n) {
+        for(int i = 0; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (i -1)*2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }//end spira
+
+    private static void npira(int n) {
+        for(int i = 0; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (i -1)*2 + 1; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }//end npira
+
 
 
     public static void main(String[] args) {
@@ -66,6 +91,8 @@ public class TriangleLB {
         triangleLU(n);
         triangleRU(n);
         triangleRB(n);
+        spira(n);
+        npira(n);
     }
 
 }
