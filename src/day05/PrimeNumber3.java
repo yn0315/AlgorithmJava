81b611a6b27fe9b026016aff34089da91c4bfe1b
yn0315 <yunu0315@naver.com>
2022-04-28 16:51:08 +0900

    소수3

--- src/day05/PrimeNumber3.java
+++ src/day05/PrimeNumber3.java
@@ -1,26 +1,24 @@
 package day05;
 
-import java.util.Arrays;
-
-public class PrimeNumber2 {
-    //1000이하의 소수를 열거(버전2)
+public class PrimeNumber3 {
+    //1000이하의 소수를 열거(버전3)
 
     public static void main(String[] args) {
-        int counter = 0;            //나눗셈의 횟수
+        int counter = 0;            //곱셈과 나눗셈의 횟수
         int ptr = 0;                //찾은 소수의 개수
         int[] prime = new int[500]; //소수를 저장하는 배열
 
         prime[ptr++] = 2; //2는 소수임, prime[0]에 2를 넣고 ptr을 1로 증가시킴
+        prime[ptr++] = 3; //3은 소수임, prime[1]에 3을 넣고 ptr을 2로 증가시킴
 
-        System.out.println(ptr);
-        System.out.println("=======================================");
 
-        for (int n = 3; n <= 1000; n += 2) {//대상은 홀수만 3,5,7 ...
-            int i;
+        for (int n = 5; n <= 1000; n += 2) {//대상은 홀수만 3,5,7 ...
+            boolean flag = false;
             System.out.println("=============================");
 
-            for (i = 1; i < ptr; i++) {//이미 찾은 소수로 나누어 봄
-                counter++;//n이 3일때 ptr이 1이라 반복문 안 돎
+            for (int i = 1; prime[i] * prime[i] <= n; i++) {//이미 찾은 소수로 나누어 봄
+                counter += 2;//곱셈과 나눗셈의 횟수이므로 두개씩 증가시킴
+
                 /////////////////////////////////////////////////
                 System.out.println("ptr = " + ptr);
                 if(ptr == 2) {
@@ -28,13 +26,17 @@ public class PrimeNumber2 {
                 }
                 System.out.println("i = "+ i);
                 ////////////////////////////////////////////////
+
                 if (n % prime[i] == 0) { //나누어떨어지면 소수가 아니므로
+                    flag = true;
                     break;              //더이상 반복은 불필요
                 }
             }//end inner for
 
-            if (ptr == i) {          //마지막까지 나누어떨어지지 않음
-                prime[ptr++] = n;   //소수라고 배열에 저장
+            if (!flag) {          //마지막까지 나누어떨어지지 않음
+                prime[ptr++] = n; //소수라고 배열에 저장
+                counter++;        //prime[i]*prime[i]<=n이 성립하지 않는 경우 for문으로 들어가지 않으므로 counter를 여기서 포함시킴
+
             }
         }//end outer for
         for (int i = 0; i < ptr; i++) {
