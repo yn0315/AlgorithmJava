7c5ccbf6a2ec9a74468cdaf8e96335f21c1517eb
yn0315 <yunu0315@naver.com>
2022-04-30 21:28:20 +0900

    퀴즈

--- src/day07/PhysicalExaminationQuiz.java
+++ src/day07/PhysicalExaminationQuiz.java
@@ -2,7 +2,7 @@ package day07;
 
 import java.util.Scanner;
 
-public class PhysicalExamination {
+public class PhysicalExaminationQuiz {
     //신체검사 데이터용 클래스 배열에서 평균 키와 시력의 분포를 구함
     static final int VMAX = 21; //시력분포(0.0에서 0.1단위로 21개)
 
@@ -21,7 +21,7 @@ public class PhysicalExamination {
     }//end PhyscData
 
     //키의 평균값을 구함
-    static double aveHeight(PhyscData[] dat) {
+    static double aveHeight(PhysicalExamination.PhyscData[] dat) {
         double sum = 0;
 
         for (int i = 0; i < dat.length; i++) {
@@ -31,7 +31,7 @@ public class PhysicalExamination {
     }//end aveHeight
 
     //시력분포를 구함
-    static void distVision(PhyscData[] dat, int[] dist) {
+    static void distVision(PhysicalExamination.PhyscData[] dat, int[] dist) {
         int i = 0;
 
         dist[i] = 0;
@@ -42,18 +42,26 @@ public class PhysicalExamination {
         }
     }//end distVision
 
+    public static void printStar(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("*");
+        }
+
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        PhyscData[] x = {
-                new PhyscData("박현규", 162, 0.3),
-                new PhyscData("함진아", 173, 0.7),
-                new PhyscData("최윤미", 175, 2.0),
-                new PhyscData("홍연의", 171, 1.5),
-                new PhyscData("이수진", 168, 0.4),
-                new PhyscData("김영준", 174, 1.2),
-                new PhyscData("박용규", 169, 0.8),
+        PhysicalExamination.PhyscData[] x = {
+                new PhysicalExamination.PhyscData("박현규", 162, 0.3),
+                new PhysicalExamination.PhyscData("함진아", 173, 0.3),
+                new PhysicalExamination.PhyscData("최윤미", 175, 2.0),
+                new PhysicalExamination.PhyscData("홍연의", 171, 1.5),
+                new PhysicalExamination.PhyscData("이수진", 168, 0.4),
+                new PhysicalExamination.PhyscData("김영준", 174, 1.2),
+                new PhysicalExamination.PhyscData("박용규", 169, 0.8),
         };
         int[] vdist = new int[VMAX]; //시력분포
 
@@ -69,10 +77,10 @@ public class PhysicalExamination {
 
         System.out.println("\n시력분포");
         for (int i = 0; i < VMAX; i++) {
-            System.out.printf("%3.1f~: %2d명\n", i / 10.0, vdist[i]);
-
+            System.out.printf("%3.1f~: ", i / 10.0);
+            printStar(vdist[i]);
+            System.out.println();
         }
 
     }
-
 }
