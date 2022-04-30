768d5383783d8de438ad491ae7c69b59b6cf7959
yn0315 <yunu0315@naver.com>
2022-04-30 20:52:18 +0900

    클래스

---
+++ src/day07/XYZ.java
@@ -0,0 +1,14 @@
+package day07;
+
+public class XYZ {
+    int x; //x,y,z는 필드
+    long y;
+    double z;
+
+    public static void main(String[] args) {
+        XYZ a;//XYZ 형의 클래스형 변수 a를 선언
+        a = new XYZ(); //XYZ형의 클래스 인스턴스(실체)를 생성
+
+        XYZ b = new XYZ(); //변수와 인스턴스 생성을 한꺼번에 선언
+    }
+}
