33bbb3975cbd2a156f8cdc2006e5d5cd42d84f89
yn0315 <yunu0315@naver.com>
2022-05-02 21:27:08 +0900

    선형탐색

---
+++ src/day08/SeqSearch.java
@@ -0,0 +1,47 @@
+package day08;
+
+import java.util.Scanner;
+
+public class SeqSearch {
+    //길이가 n인 배열 a에서 key와 같은 요소를 선형 검색하기
+    static int seqSearch(int[] a, int n, int key) {
+        int i = 0;
+
+        while (true) {
+            if(i == n) {
+                return -1;
+            }//검색 실패(-1반환)
+            if(a[i] == key) {
+                return i;
+            }//검색 성공(인덱스반환)
+            i++;
+        }
+
+    }//end seqSearch
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int num = sc.nextInt();
+        int[] x = new int[num]; //길이가 num인 배열
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("x[" + i + "]: " );
+            x[i] = sc.nextInt();
+        }
+
+        System.out.print("검색할 값: ");
+        int ky = sc.nextInt();
+        int idx = seqSearch(x, num, ky); //배열 x에서 키값이 ky인 요소를 검색
+
+        if(idx == -1) {
+            System.out.println("그 값의 요소가 없습니다.");
+        }else {
+            System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+        }
+
+    }//end main
+
+
+}//end class
