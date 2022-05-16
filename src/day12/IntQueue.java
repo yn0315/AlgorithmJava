064af2ad25ff21dd10a9540ec7179dabf6ce1eda
yn0315 <yunu0315@naver.com>
2022-05-16 22:48:28 +0900

    queue tester

--- src/day12/IntQueue.java
+++ src/day12/IntQueue.java
@@ -53,5 +53,61 @@ public class IntQueue {
         return x;
     }
 
+    //큐에서 데이터를 피크(프런트 데이터를 들여다봄)
+    public int peek() throws EmptyIntQueueException{
+        if(num <= 0) {
+            throw new EmptyIntQueueException(); //큐가 비어있음
+        }
+        return que[front];
+    }
+
+    //큐에서 x를 검색하여 인덱스(찾지 못하면 -1)를 반환
+    public int indexOf(int x) {
+        for (int i = 0; i < num; i++) {
+            int idx = (i + front) % max; //검색성공
+            if(que[idx] == x) {
+                return idx;
+            }
+        }
+        return -1; //검색실패
+    }
+
+    //큐를 비움
+    public void clear() {
+        num = front = rear = 0;
+    }
+
+    //큐의 용량을 반환
+    public int capacity() {
+        return max;
+    }
+
+    //큐에 쌓여있는 데이터 수를 반환
+    public int size() {
+        return num;
+    }
+
+    //큐가 비어있나?
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    //큐가 가득 찼나?
+    public boolean isFull() {
+        return num >= max;
+    }
+
+    //큐 안의 모든 데이터를 프런트 -> 리어 순으로 출력
+    public void dump() {
+        if(num <= 0) {
+            System.out.println("큐가 비어있습니다.");
+        }else {
+            for (int i = 0; i < num; i++) {
+                System.out.print(que[(i + front) % max] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
 }
