366f53e111b11f83e1f6599387653ca492bc41e6
yn0315 <yunu0315@naver.com>
2022-06-02 13:19:59 +0900

    Deque 완성

--- src/day12/IntDeque.java
+++ src/day12/IntDeque.java
@@ -1,6 +1,10 @@
 package day12;
 
+import java.util.Deque;
+
 public class IntDeque {
+
+    ///////////rear----------------------------front////////////////
     private int max; //덱의 용량
     private int num; //현재 데이터 수
     private int front; //맨 앞 커서
@@ -37,7 +41,7 @@ public class IntDeque {
         num++;
         if(--front < 0)
             front = max - 1;
-        que[front] = x;//////////////////////////?그냥 front라고..?!
+        que[front] = x;/////////////////////////////////////////////
         return x;
     }
 
@@ -52,7 +56,94 @@ public class IntDeque {
         return x;
     }
 
+    //덱의 머리쪽부터 디큐
+    public int dequeFront() throws EmptyIntDequeException {
+        if(num <= 0)
+            throw new EmptyIntDequeException();
+        int x = que[front++];
+        num--;
+        if(front == max)
+            front = 0;
+        return x;
+    }
 
+    //덱의 꼬리쪽부터 디큐
+    public int dequeRear() throws EmptyIntDequeException {
+        if(num <= 0)
+            throw new EmptyIntDequeException();
+        num--;
+        if(--rear < 0)
+           rear = max - 1;
+        return que[rear];/////////////////////////
+    }
+
+    // 덱(deck)의 머리부터 데이터를 피크(머리데이터를 살펴봄)
+    public int peekFront() throws EmptyIntDequeException{
+        if(num <= 0)
+            throw new EmptyIntDequeException();
+        return que[front];
+    }
+
+    // 덱(deck)의 꼬리부터 데이터를 피크(꼬리데이터를 살펴봄)
+    public int peekRear() throws EmptyIntDequeException{
+        if(num <= 0)
+            throw new EmptyIntDequeException();
+        return que[rear == 0 ? max - 1 : rear - 1];/////////
+    }
+
+    // 덱(deck)에서 x를 검색하여 index(찾지 못하면 -1)를 반환
+    public int indexOf(int x) {
+        for (int i = 0; i < num; i++) {
+            if(que[(i + front) % max] == x)
+                return i + front; //검색성공
+        }
+        return -1;
+    }
+
+    // 덱(deck)에서 x를 검색하여 머리부터 몇 번 째인가(찾지 못하면 0)를 반환
+    public int search(int x) {
+        for (int i = 0; i < num; i++) {
+            if(que[(i + front) % max] == x)
+                return i + 1; //검색성공
+        }
+        return 0;
+    }
+
+    // 덱(deck)을 비움
+    public void clear() {
+        rear = front = num = 0;
+    }
+
+    // 덱(deck)의 용량을 반환
+    public int capacity() {
+        return max;
+    }
+
+    // 덱(deck)에 쌓인 데이터 수를 반환
+    public int size() {
+        return num;
+    }
+
+    // 덱(deck)이 비어 있는가?
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    // 덱(deck)이 가득 찼는가?
+    public boolean isFull() {
+        return num >= max;
+    }
+
+    // 덱(deck)내의 데이터를 머리→꼬리의 차례로 나타냄
+    public void dump() {
+        if(num <= 0)
+            System.out.println("덱이 비었습니다.");
+        for (int i = 0; i < num; i++) {
+            System.out.print(que[(i + front) % max] + " ");
+        }System.out.println();
+    }
+    
+//    Deque
 
 
 }
